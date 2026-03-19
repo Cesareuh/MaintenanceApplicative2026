c71391fc21a8650872e41f3a3f1ed9b0e85ef5fa
Mathieu <mathieu.graff498@gmail.com>
2026-03-19 08:05:18 +0100

    refacto

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -7,55 +7,57 @@ class GildedRose {
         this.items = items;
     }
 
+    public void doBrie(int i) {
+        items[i].sellIn = items[i].sellIn - 1;
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1;
+            if (items[i].sellIn < 0) {
+                items[i].quality = items[i].quality + 1;
+            }
+        }
+    }
+
+    public void doConcert(int i) {
+        items[i].sellIn = items[i].sellIn - 1;
+        items[i].quality = items[i].quality + 1;
+
+        if (items[i].quality < 50) {
+            if (items[i].sellIn < 6) {
+                items[i].quality = items[i].quality + 1;
+            }
+            if (items[i].sellIn < 11) {
+                items[i].quality = items[i].quality + 1;
+            }
+        }
+        if (items[i].sellIn < 0) {
+            items[i].quality = 0;
+        }
+    }
+
+    public void doSulfuras(int i) {
+
+    }
+
+    public void doOther(int i) {
+        items[i].sellIn = items[i].sellIn - 1;
+        if (items[i].quality > 0) {
+            items[i].quality = items[i].quality - 1;
+            if (items[i].sellIn < 0) {
+                items[i].quality = items[i].quality - 1;
+            }
+        }
+    }
+
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+            if (items[i].name.equals("Aged Brie")) {
+                doBrie(i);
+            } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                doConcert(i);
+            } else if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                doSulfuras(i);
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+                doOther(i);
             }
         }
     }
--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    /*
     @Test
     void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
+    }
+    */
+
+    @Test
+    void agedBrie() {
+        Item[] items = new Item[] {
+                new Item("Aged Brie", 0, 0),
+                new Item("Aged Brie", 0, 100),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 100, 0),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 100, 49),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 0),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 100),
+                new Item("Sulfuras, Hand of Ragnaros", -1, 100),
+                new Item("Sulfuras, Hand of Ragnaros", 100, 100),
+                new Item("autre", 0, 0),
+                new Item("autre", 0, 100),
+                new Item("Aged Brie", 2, 0),
+                new Item("autre", 2, 100),
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie, -1, 2", app.items[0].toString());
+        assertEquals("Aged Brie, -1, 100", app.items[1].toString());
+        assertEquals("Backstage passes to a TAFKAL80ETC concert, -1, 0", app.items[2].toString());
+        assertEquals("Backstage passes to a TAFKAL80ETC concert, -1, 0", app.items[3].toString());
+        assertEquals("Backstage passes to a TAFKAL80ETC concert, 99, 1", app.items[4].toString());
+        assertEquals("Backstage passes to a TAFKAL80ETC concert, 99, 50", app.items[5].toString());
+        assertEquals("Sulfuras, Hand of Ragnaros, 0, 0", app.items[6].toString());
+        assertEquals("Sulfuras, Hand of Ragnaros, 0, 100", app.items[7].toString());
+        assertEquals("Sulfuras, Hand of Ragnaros, -1, 100", app.items[8].toString());
+        assertEquals("Sulfuras, Hand of Ragnaros, 100, 100", app.items[9].toString());
+        assertEquals("autre, -1, 0", app.items[10].toString());
+        assertEquals("autre, -1, 98", app.items[11].toString());
+        /*
+        assertEquals("Aged Brie", app.items[0].name);
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[2].name);
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[4].name);
+        assertEquals("autre", app.items[6].name);
+        */
     }
 
 }
